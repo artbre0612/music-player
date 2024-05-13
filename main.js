ccfac02ab39de0901e195cf02967bd68b03394fd
Arthur <arthur4456645@gmail.com>
2024-05-13 18:13:35 +0800

    deploy

---
+++ main.js
@@ -0,0 +1,92 @@
+const musicContainer = document.querySelector('.music-container')
+const playBtn = document.querySelector('#play')
+const prevBtn = document.querySelector('#prev')
+const nextBtn = document.querySelector('#next')
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
+const title = document.querySelector('#title')
+const musicCover = document.querySelector('#music-cover')
+
+//song titles
+const songs = ['BetterDay', 'GoodNight', 'LofiChill', 'OnceInParis', 'Smooth']
+
+//key track of songs
+let songIndex = 1
+
+//init load song into dom
+loadSong(songs[songIndex])
+
+//Update song details
+function loadSong(song) {
+  title.innerText = song
+  audio.src = `music/${song}.mp3`
+  musicCover.src = `image/${song}.jpg`
+}
+
+function pauseSong() {
+  musicContainer.classList.remove('play')
+  playBtn.querySelector('.fa-solid').classList.add('fa-play')
+  playBtn.querySelector('.fa-solid').classList.remove('fa-pause')
+  audio.pause()
+}
+
+function playSong() {
+  musicContainer.classList.add('play')
+  playBtn.querySelector('.fa-solid').classList.remove('fa-play')
+  playBtn.querySelector('.fa-solid').classList.add('fa-pause')
+  audio.play()
+}
+
+function prevSong() {
+  songIndex = songIndex -= 1
+  if (songIndex < 0) {
+    songIndex = songs.length - 1
+  }
+  loadSong(songs[songIndex])
+  playSong()
+}
+
+function nextSong() {
+  songIndex = songIndex += 1
+  if (songIndex > songs.length - 1) {
+    songIndex = 0
+  }
+  loadSong(songs[songIndex])
+  playSong()
+}
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement
+  const progressPercent = (currentTime / duration) * 100
+  progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+  const width = this.clientWidth
+  const clickX = e.offsetX
+  const duration = audio.duration
+
+  audio.currentTime = (clickX / width) * duration
+}
+
+//event listener
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play')
+
+  if (isPlaying) {
+    pauseSong()
+  } else {
+    playSong()
+  }
+})
+
+//change song events
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
